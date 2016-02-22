51d69fc8db61d2c2846975800c59b560ab860b2e
gantta <gantta@gmail.com>
2016-02-22 18:49:45 -0500

    ver update

--- lambda/writeSBSUnit/index.js
+++ lambda/writeSBSUnit/index.js
@@ -5,7 +5,7 @@ var UNIT_TABLE = 'GrovePi-SBSUnitTable-ZM5492IW4WIM';
 var ddb = new AWS.DynamoDB();
 
 exports.handler = function (event, context) {
-  console.log("writeSBS Data Version 1.1");
+  console.log("writeSBS Unit Version 1.2");
   console.log("SBSID:", event.sbsid);
   console.log("Putting data into DynamoDB:", event);
   ddb.putItem({
